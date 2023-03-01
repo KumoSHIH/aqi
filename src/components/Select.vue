49ead9646a5a5e79477feba555ed549027a68e0d
DESKTOP-GSNJ48I\user008 <kumo@percentage.asia>
2023-03-01 18:42:24 +0800

    0301

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,71 +1,28 @@
-<script setup>
-import axios from 'axios';
+<script>
 import { toRaw, onMounted, reactive, ref, computed } from 'vue';
 
-const data = reactive([]);
-
-const cityList = reactive([]);
-
-// const json = ref([]);
-let city = ref([]);
-
-
-// newList.value.forEach((el) => {
-//   console.log(el);
-// })
-// cityList.value = newList.map((el) => el.county === '高雄市')
-// console.log(cityList);
-
-onMounted(() => {
-  getData();
-})
-
-
-
-// 取得 api 資料存放
-let getData = async () => {
-  const url = 'https://data.epa.gov.tw/api/v2/aqx_p_02';
-  const key = '19843a99-912f-4804-9d4b-3fa43961f7a0';
-  // let setData = await function(res) {
-  //   data.value = res.data;
-  //   // console.log(data);
-  // }
-
-  // await axios.get(`${url}?api_key=${key}`)
-  //   .then((res) => {
-  //     // console.log(res);
-  //     setData(res);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   })
-
-  const res = await axios.get(`${url}?api_key=${key}`)
-    .then((res) => {
-      Object.assign(data, res.data)
-      // city = toRaw(data.records)
-    })  
-    .catch((error) => {
-      console.log(error);
-    })
+export default {
+  props: {
+    location: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  setup(props) {
+    return {
+      props
+    }
+  }
 }
-
-console.log(data);
-
-const filterData = computed(() => {
-  // return city
-})
-
 
 </script>
 
 <template>
   <div>
     <select name="" id="">
-      <option value="">123</option>
+      <option value="">請選擇</option>
+      <option v-for="item in props.location" :key="item" :value="item">{{ item }}</option>
     </select>
-    <p>{{ cityList }}</p>
-    <p>{{ filterData }}</p>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,45 @@
-<script>
-import HelloWorld from '@/components/HelloWorld.vue';
-import Select from '@/components/Select.vue';
-import axios from 'axios';
-import { onMounted, reactive, ref } from 'vue';
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import Select from '@/components/Select.vue';
+  import axios from 'axios';
+  import { onMounted, reactive, ref } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    Select,
-  },
-  setup() {
-    const data = reactive({});
-    const cityList = reactive([]);
-    
-    onMounted(() => {
-      const url = 'https://data.epa.gov.tw/api/v2/aqx_p_02';
-      const key = '19843a99-912f-4804-9d4b-3fa43961f7a0';
 
-      axios.get(`${url}?api_key=${key}`)
-        .then((res) => {
-          // console.log(res);
-          data.value = res.data.records;
+  const data = [];
+  const location = [];
+  const filterText = ref('all');
+
+  onMounted(() => {
+    getData();
+  })
+
+  let getData = async () => {
+    const url = 'https://data.epa.gov.tw/api/v2/aqx_p_02';
+    const key = '19843a99-912f-4804-9d4b-3fa43961f7a0';
+
+    const res = await axios.get(`${url}?api_key=${key}`)
+      .then((res) => {
+        Object.assign(data, res.data.records);
+
+        data.forEach((el) => {
+          if(location.indexOf(el.county) === -1) location.push(el.county)
         })
-        .catch((error) => {
-          console.log(error);
-        })
-    })
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
-    return {
-      data
-    }
-  },
-}
+  // console.log(location);
+
 </script>
 
 <template>
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Select />
+    <Select :location="location" />
   </div>
 </template>
 
